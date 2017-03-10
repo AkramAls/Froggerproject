e7d52ac8e4729371678098af990da07947e49b98
Jpollock2474 <Jpollock2474@gmail.com>
2017-03-10 03:47:47 -0600

    Updated GamePanel
    Updated GamePanel to include sound effect functionality. Changed the layout of the movement buttons to be easier to handle, and moved them to the left of the screen. Added an action button. Resized and positioned UI elements to better fit our decided tablet.

--- app/src/main/java/cinnamontoastcrunch/froggergame/GamePanel.java
+++ app/src/main/java/cinnamontoastcrunch/froggergame/GamePanel.java
@@ -1,17 +1,17 @@
 package cinnamontoastcrunch.froggergame;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.view.Display;
+import android.media.MediaPlayer;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.WindowManager;
+
+import java.util.Random;
 
 /**
  * Created by Jpollock on 3/1/2017.
@@ -23,18 +23,48 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
 
     private Player player;
+
     private MovementButton upButton;
     private MovementButton downButton;
     private MovementButton leftButton;
     private MovementButton rightButton;
+    private MovementButton actionButton;
+
     private Point playerPoint;
+
     private Point upPoint;
     private Point downPoint;
     private Point leftPoint;
     private Point rightPoint;
+    private Point actionPoint;
+
     private static int MOVE_SPEED = 15; // This determines the number of steps before reaching the end of the screen.
     private static int X_SPEED = ((getScreenWidth()/MOVE_SPEED)); // We scale the speed with screen size so bigger screens wont have more space.
     private static int Y_SPEED = ((getScreenWidth()/MOVE_SPEED));
+
+    MediaPlayer randomCroak = MediaPlayer.create(getContext(), R.raw.croak);
+
+    Random rand = new Random();
+    int random;
+
+
+    private void croak() {
+
+        randomCroak.release();
+
+        random = (int)(Math.random() * 3 + 1);
+        switch (random) {
+            case 1: randomCroak = MediaPlayer.create(getContext(), R.raw.croak);
+                    randomCroak.start();
+                    break;
+            case 2: randomCroak = MediaPlayer.create(getContext(), R.raw.ribbit);
+                    randomCroak.start();
+                    break;
+            default: randomCroak = MediaPlayer.create(getContext(), R.raw.ribbit2);
+                    randomCroak.start();
+                    break;
+        }
+    }
 
 
     public static int getScreenWidth() {
@@ -53,20 +83,23 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
         thread = new MainThread(getHolder(), this);
 
-        player = new Player(new Rect(100, 100, 200, 200), Color.rgb(0, 255, 0)); // Starting size and color. GREEN!
+        player = new Player(new Rect(100, 100, 175, 175), Color.rgb(0, 255, 0)); // Starting size and color. GREEN!
         playerPoint = new Point(150, 150); // Starting position.
 
-        upButton = new MovementButton(new Rect(100, 100, 250, 250), Color.rgb(75, 75, 75));
-        upPoint = new Point((getScreenWidth()/2), (getScreenHeight()-300)); // This allows the button to always be located in the same spot, regardless of screen size.
+        upButton = new MovementButton(new Rect(100, 100, 200, 200), Color.rgb(75, 75, 75));
+        upPoint = new Point((175), (getScreenHeight()-275)); // This allows the button to always be located in the same spot, regardless of screen size.
 
-        downButton = new MovementButton(new Rect(100, 100, 250, 250), Color.rgb(75, 75, 75));
-        downPoint = new Point((getScreenWidth()/2), (getScreenHeight()-100));
+        downButton = new MovementButton(new Rect(100, 100, 200, 200), Color.rgb(75, 75, 75));
+        downPoint = new Point((175), (getScreenHeight()-75));
 
-        leftButton = new MovementButton(new Rect(100, 100, 250, 250), Color.rgb(75, 75, 75));
-        leftPoint = new Point(((getScreenWidth()/2)-200), (getScreenHeight()-100));
+        leftButton = new MovementButton(new Rect(100, 100, 200, 200), Color.rgb(75, 75, 75));
+        leftPoint = new Point((75), (getScreenHeight()-175));
 
-        rightButton = new MovementButton(new Rect(100, 100, 250, 250), Color.rgb(75, 75, 75));
-        rightPoint = new Point(((getScreenWidth()/2)+200), (getScreenHeight()-100));
+        rightButton = new MovementButton(new Rect(100, 100, 200, 200), Color.rgb(75, 75, 75));
+        rightPoint = new Point((275), (getScreenHeight()-175));
+
+        actionButton = new MovementButton(new Rect(100, 100, 300, 300), Color.RED);
+        actionPoint = new Point((getScreenWidth()-175), getScreenHeight()-175);
 
         setFocusable(true);
 
@@ -137,7 +170,10 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
                     playerPoint.offset(X_SPEED, 0);
                  }
 
-            playSoundEffect(0);
+                if (actionButton.button.contains((int)event.getX(), (int)event.getY())) {
+                    // TODO: Add ability trigger here.
+                    croak();
+                }
 
         }
 
@@ -151,6 +187,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         downButton.update(downPoint);
         leftButton.update(leftPoint);
         rightButton.update(rightPoint);
+        actionButton.update(actionPoint);
     }
 
     @Override
@@ -165,6 +202,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         downButton.draw(canvas); // Draw the down button to the screen.
         leftButton.draw(canvas); // Draw the left button to the screen.
         rightButton.draw(canvas); // Draw the right button to the screen.
+        actionButton.draw(canvas); // Draw the action button to the screen.
 
         //System.out.println(getScreenHeight());
         //System.out.println(getScreenWidth());
